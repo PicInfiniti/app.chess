62fe5ffdb04caf3c281250b5250bbdb0e789645a
Lenovo <picinfiniti@gmail.com>
2023-10-01 14:05:41 +0330

    update number

--- src/assets/js/ui/setup.js
+++ src/assets/js/ui/setup.js
@@ -42,8 +42,8 @@ $.each(stockpile, function (key, value) {
 
 for (let i = 0; i < 8; i++) {
   for (let j = 0; j < 8; j++) {
-    
-    if((i+j)%2==0){
+
+    if ((i + j) % 2 == 0) {
       $('#fake').append("<label type='text' style='background-color: #ffba75'>0</label>")
       $('#board').append(`<label type='text' id='b-${i}-${j}' name='${i}-${j}' style='background-color: #ffba75'>${i}${j}</label>`)
     } else {
@@ -52,12 +52,12 @@ for (let i = 0; i < 8; i++) {
     }
   }
 }
-
-for (let i = 1; i < 9; i++) {
-  $('arena .top').append(`<div><b>${i}</b></div>`)
-  $('arena .bottom').append(`<div><b>${i}</b></div>`)
-  $('arena .right').append(`<div><b>${9-i}</b></div>`)
-  $('arena .left').append(`<div><b>${9-i}</b></div>`)
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+for (let i = 0; i < 8; i++) {
+  $('arena .top').append(`<div><b>${alphabet[i]}</b></div>`)
+  $('arena .bottom').append(`<div><b>${alphabet[i]}</b></div>`)
+  $('arena .right').append(`<div><b>${8-i}</b></div>`)
+  $('arena .left').append(`<div><b>${8-i}</b></div>`)
 }
 update_board()
 // const content = document.querySelector('body');
